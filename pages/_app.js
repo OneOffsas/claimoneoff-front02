2e87a6f366b0d5da32eb11fe2e8c79849f094bab
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-16 00:08:28 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,18 @@
-import '../styles/globals.css'
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import Head from "next/head";
+import "../styles/globals.css"; // Garde-le si tu veux customiser après
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
