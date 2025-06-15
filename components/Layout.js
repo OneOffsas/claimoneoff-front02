83bd7168ad69b288d5b910480ee091154661aea0
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-15 16:23:05 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,1 +1,15 @@
+import Sidebar from './Sidebar';
+import Header from './Header';
+
+export default function Layout({ children }) {
+  return (
+    <div style={{ display: "flex", minHeight: "100vh", background: "#f7f8fa" }}>
+      <Sidebar />
+      <div style={{ flex: 1, display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+        <Header />
+        <main style={{ flex: 1, padding: 40 }}>{children}</main>
+      </div>
+    </div>
+  );
+}
 
