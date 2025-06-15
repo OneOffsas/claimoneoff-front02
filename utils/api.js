c08a01cfb5536257ff522959e7959e05a06110e1
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-15 18:23:00 +0200

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,8 @@
-const API_URL = "/api/register";
-export async function apiCall(action, data) {
+// /utils/api.js
+export const API_URL = "https://script.google.com/macros/s/AKfycbyqgEfqr7VcWjw75Q4O5KBew3L2gbHL3_I8vMsz_mo1wsHKxnDTCixAFN4RQTC_3_xS/exec";
+
+// Fonction utilitaire pour appeler l’API
+export async function apiCall(action, data = {}) {
   const res = await fetch(API_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
