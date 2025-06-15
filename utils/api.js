f18995e538df0015bbf840fcef4c9a634a77c0f3
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-15 22:36:12 +0200

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,8 @@
-export const API_URL = "https://billowing-base-6a8c.oneoffsas.workers.dev/";
+// /utils/api.js
 
+export const API_URL = "https://script.google.com/macros/s/AKfycbz4oaV2F4-DeHC4-oYaR8wiTgha1ROTXN1WAMQT9H72SPI6b1NCtlClxZ8WwR0f6rZ9lg/exec";
+
+// Fonction générique d'appel à l'API
 export async function apiCall(action, data) {
   const res = await fetch(API_URL, {
     method: "POST",
