b57665d15027159b8827cf70ec5c08ba8bcfaf8c
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-16 14:16:56 +0200

    Create auth.js

---
+++ utils/auth.js
@@ -0,0 +1,27 @@
+// utils/auth.js
+
+export function saveUser(userObj) {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("user", JSON.stringify(userObj));
+  }
+}
+
+export function getUser() {
+  if (typeof window !== "undefined") {
+    const u = localStorage.getItem("user");
+    if (u) {
+      try {
+        return JSON.parse(u);
+      } catch(e) {
+        return null;
+      }
+    }
+  }
+  return null;
+}
+
+export function clearUser() {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("user");
+  }
+}
