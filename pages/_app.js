c0182bc87a176dc337669f47d1e273fe695aa21a
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-16 17:18:29 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 // pages/_app.js
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../styles/globals.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "../styles/globals.css"
+import Layout from "../components/Layout"
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  // pageProps.user doit venir de votre authentif si vous enregistrez user  
+  return (
+    <Layout user={pageProps.user}>
+      <Component {...pageProps}/>
+    </Layout>
+  )
 }
