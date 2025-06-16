54f43945afe55afdc830fcf95fce49fe5c088745
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-16 17:19:08 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
 // pages/index.js
-import Link from 'next/link';
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 
 export default function Home() {
-  return (
-    <div className="d-flex justify-content-center align-items-center vh-100" style={{ backgroundColor: '#f3f4f6' }}>
-      <div className="text-center p-4 bg-white shadow card-custom">
-        <h1 className="mb-4">Bienvenue sur ClaimOneOff</h1>
-        <p className="mb-4">Connectez-vous ou créez un compte pour accéder au système de ticketing.</p>
-        <Link href="/login" passHref>
-          <a className="btn btn-primary-custom btn-lg mb-2 w-100">Se connecter</a>
-        </Link>
-        <Link href="/register" passHref>
-          <a className="btn btn-outline-primary w-100">Créer un compte</a>
-        </Link>
-      </div>
-    </div>
-  );
+  const router = useRouter()
+  useEffect(() => { router.replace("/login") }, [])
+  return null
 }
