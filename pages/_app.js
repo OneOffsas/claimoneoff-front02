22e2e50fab4e9dc95ee99d5b811d95955bcff0f0
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-16 14:27:20 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 // pages/_app.js
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import global du CSS Bootstrap
-import '@/styles/globals.css'; // si tu gardes Tailwind ou autres styles globaux
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';  // Chemin relatif depuis pages/_app.js
 
 export default function MyApp({ Component, pageProps }) {
-  // Tu peux ajouter ici du code global, par ex. pour analytics, auth check, etc.
-  // Exemple : gestion redirection si non authentifié, mais on gère ça dans chaque page.
   return <Component {...pageProps} />;
 }
