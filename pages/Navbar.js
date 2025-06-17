bb422bb1d40b22f1513ed743635266dcae2a40a3
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-17 17:24:46 +0200

    Create Navbar.js

---
+++ pages/Navbar.js
@@ -0,0 +1,39 @@
+// components/Navbar.js
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function Navbar() {
+  const router = useRouter();
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+      <div className="container">
+        <Link href="/" className="navbar-brand">ClaimOneOff</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNav"
+          aria-controls="mainNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"/>
+        </button>
+        <div className="collapse navbar-collapse" id="mainNav">
+          <ul className="navbar-nav ms-auto">
+            {router.pathname !== '/login' && (
+              <li className="nav-item">
+                <Link href="/login" className="nav-link">Se connecter</Link>
+              </li>
+            )}
+            {router.pathname !== '/register' && (
+              <li className="nav-item">
+                <Link href="/register" className="nav-link">S’inscrire</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
