6812b44bbebe338375be15ad807d071826f6120f
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-21 14:53:18 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
+import NotificationProvider from "../components/NotificationProvider";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <NotificationProvider>
+      <Component {...pageProps} />
+    </NotificationProvider>
+  );
 }
