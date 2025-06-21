a7fad9cdba687d23e0d6f16ee6c4f664bbfc6738
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-21 14:37:31 +0200

    Update UsersAdmin.js

--- components/UsersAdmin.js
+++ components/UsersAdmin.js
@@ -1,17 +1,3 @@
-// /components/Profile.js
-export default function Profile({ user }) {
-  return (
-    <div className="card shadow border-0 p-4">
-      <h3>Mon Profil</h3>
-      <p><b>Nom :</b> {user.nom}</p>
-      <p><b>Prénom :</b> {user.prenom}</p>
-      <p><b>Email :</b> {user.email}</p>
-      <p><b>Société :</b> {user.societe}</p>
-      <p><b>Rôle :</b> {user.role}</p>
-    </div>
-  );
-}
-
 // /components/UsersAdmin.js
 export default function UsersAdmin() {
   return (
