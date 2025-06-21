5771e4ac4130a09edde64b3b6b19b1a252e9053c
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-21 14:45:09 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect } from 'react';
+import "../styles/global.css";
+
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
   return <Component {...pageProps} />;
 }
