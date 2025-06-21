8ee57d94ec4509ba997147992dc95058c0dd4ad2
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-21 14:45:44 +0200

    Update AdminCockpit.js

--- components/AdminCockpit.js
+++ components/AdminCockpit.js
@@ -1,12 +1,13 @@
-import AdminTickets from "./AdminTickets";
 import StatsDashboard from "./StatsDashboard";
+import AdminTickets from "./AdminTickets";
 import UsersAdmin from "./UsersAdmin";
 import Profile from "./Profile";
 
 export default function AdminCockpit({ user, page }) {
+  if (page === "dashboard") return <StatsDashboard user={user} admin />;
   if (page === "tickets") return <AdminTickets user={user} />;
   if (page === "stats") return <StatsDashboard user={user} admin />;
   if (page === "users") return <UsersAdmin />;
   if (page === "profile") return <Profile user={user} />;
-  return <StatsDashboard user={user} admin />;
+  return null;
 }
