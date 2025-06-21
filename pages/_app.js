d15095e0c9cdf042b6ae6e2434cda2f53a8aaf1d
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-21 15:00:16 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
+// pages/_app.js
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
 import NotificationProvider from "../components/NotificationProvider";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <NotificationProvider>
+      {/* On charge js-sha256 AVANT toute page */}
+      <Script
+        src="https://cdn.jsdelivr.net/npm/js-sha256@0.9.0/build/sha256.min.js"
+        strategy="beforeInteractive"
+        onLoad={() => { console.log("SHA256 loaded"); }}
+      />
       <Component {...pageProps} />
     </NotificationProvider>
   );
