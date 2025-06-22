961396fa26469b1a89f1c503555a6bd759185883
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-22 16:37:42 +0200

    Update AdminLayout.js

--- components/AdminLayout.js
+++ components/AdminLayout.js
@@ -1,9 +1,10 @@
 import AdminSidebar from "./AdminSidebar";
+
 export default function AdminLayout({ children, current }) {
   return (
-    <div className="flex h-screen bg-gray-100">
+    <div className="flex bg-gray-100 min-h-screen">
       <AdminSidebar current={current} />
-      <main className="flex-1 p-8 overflow-y-auto">{children}</main>
+      <main className="ml-64 w-full p-10">{children}</main>
     </div>
   );
 }
