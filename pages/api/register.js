a69f2acfcada8ef8877eef43ba4a7492216417e1
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-22 23:23:51 +0200

    Update register.js

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,12 +1,6 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method Not Allowed" }); // 405 = Méthode non autorisée
+    return res.status(405).json({ error: "Méthode non autorisée" });
   }
-
-  // -------------- ICI LE VRAI TRAITEMENT --------------
-
-  // Par exemple, juste pour tester :
-  // return res.status(200).json({ message: "Inscription bien reçue !" });
-
-  // -------------- CODE À COMPLÉTER APRÈS --------------
+  res.status(200).json({ message: "OK POST reçu !" });
 }
