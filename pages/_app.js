5ce51e242fd92a14803968f9ccfa17e180b14981
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-22 15:46:00 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer position="top-right" autoClose={2800} hideProgressBar newestOnTop />
+    </>
+  );
 }
