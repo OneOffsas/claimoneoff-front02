0150d265ba6336c37bb1fa4cdedcd063b11921ce
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-22 17:10:29 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    // si tu utilises app directory, ajoute "./app/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
