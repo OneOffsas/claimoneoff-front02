31ba7daa1a4f68b4798e2b49d756cf66d0d0934c
OneOffsas <benjamin-ruggieri@hotmail.fr>
2025-06-22 16:39:09 +0200

    Update tickets.js

--- pages/dashboard/tickets.js
+++ pages/dashboard/tickets.js
@@ -1,10 +1,9 @@
 import AdminLayout from "../../components/AdminLayout";
 import TicketsTable from "../../components/TicketsTable";
-
 export default function TicketsPage() {
   return (
     <AdminLayout current="tickets">
-      <h1 className="text-2xl font-bold mb-4">Tous les tickets</h1>
+      <h1 className="text-2xl font-bold mb-8">Tous les tickets</h1>
       <TicketsTable />
     </AdminLayout>
   );
